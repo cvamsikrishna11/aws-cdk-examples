df1bc5e57495130c5c230e7d28edeab32f1ce5df
vamsi chunduru <vamsi chunduru>
2021-06-03 23:55:23 +0200

    added code for new AWS resouces, metadata settings, new cdk dependencies

--- first_one/first_one_stack.py
+++ first_one/first_one_stack.py
@@ -2,7 +2,8 @@
 # from aws_cdk import aws_s3 as _s3
 from aws_cdk import(
     core as cdk,
-    aws_s3 as _s3
+    aws_s3 as _s3,
+    aws_iam as _iam
 )
 
 # For consistency with other languages, `cdk` is the preferred import name for
@@ -18,11 +19,22 @@ class FirstOneStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        _s3.Bucket(
-            self,
-            id='myS3Bucket',
-            bucket_name='aws-cdk-bucket-2021',
-            versioned=False,
-            encryption=_s3.BucketEncryption.S3_MANAGED,
-            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
-        )
+        # _s3.Bucket(
+        #     self,
+        #     id='myS3Bucket',
+        #     bucket_name='aws-cdk-bucket-2021',
+        #     versioned=False,
+        #     encryption=_s3.BucketEncryption.S3_MANAGED,
+        #     block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        # )
+
+        # my_bucket = _s3.Bucket(
+        #     self,
+        #     id='myNewBucket'
+        # )
+
+        myIAMGroup = _iam.Group(self, id='IAMGroup-1',
+                                group_name='aws-cdk-group')
+
+        # output_1 = cdk.CfnOutput(self, id='my-bucket-output-1',
+        #                          value=my_bucket.bucket_name, description='The newly created bucket', export_name='my-bucket-output-1')
